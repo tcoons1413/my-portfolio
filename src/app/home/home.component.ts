702f9dfad50563112074f6b51c3c3db9a6539dcb
Thomas Coons <tomcoons1413@gmail.com>
2025-02-08 22:28:46 -0500

    Added education and work-experience components.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { EducationComponent } from '../education/education.component';
+import { WorkExperienceComponent } from '../work-experience/work-experience.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [EducationComponent,WorkExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
