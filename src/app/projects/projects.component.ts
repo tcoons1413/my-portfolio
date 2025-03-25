6ea76c542f2415a4a121bd143b4424d277663c93
Thomas Coons <tomcoons1413@gmail.com>
2025-03-25 12:03:11 -0400

    Clean up

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { CardComponent } from '../card/card.component';
 
 
 @Component({
   selector: 'app-projects',
-  imports: [CardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
